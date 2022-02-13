81b4d9c6fdfa3261d43098d72e56299331b9c0a7
sttell <st.tell@mail.ru>
2022-02-13 22:23:52 +0300

    Добавление файла c alias'ами в виде header.

---
+++ include/aliases.h
@@ -0,0 +1,11 @@
+#ifndef _INCLUDE_ALIASES_H_
+#define _INCLUDE_ALIASES_H_
+
+#include <cinttypes>
+
+// Тип используемый для выставления флага запуска модуля в виде выставленных бит.
+using module_t = uint8_t;
+// Тип хранящий тип завершения операции запуска модуля в виде выставленных бит.
+using exit_module_status = uint8_t;
+
+#endif // _INCLUDE_ALIASES_H_
