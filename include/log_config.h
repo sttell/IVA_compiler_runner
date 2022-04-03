78ca22a516d63f705c00478229b6de01e8599b64
sttell <st.tell@mail.ru>
2022-04-03 13:37:08 +0300

    Добавлена документация к log_config.h

--- include/log_config.h
+++ include/log_config.h
@@ -1,3 +1,14 @@
+/**
+ * @file log_config.h
+ * @author Ilya Biro (st.tell@mail.ru)
+ * @brief Конфигурация лога ПО.
+ * @version 1.0
+ * @date 2022-04-03
+ * 
+ * 
+ * @warning На данный момент логгирование не реализовано.
+ * @copyright Copyright (c) 2022
+ */
 #ifndef LOG_CONFIG_H
 #define LOG_CONFIG_H
 
