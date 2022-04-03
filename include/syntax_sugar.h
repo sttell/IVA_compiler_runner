d48e29acb3c2d12e37e3624aa2bd2155fc9f1f4b
sttell <st.tell@mail.ru>
2022-04-03 23:31:57 +0300

    Добавлен файл с доплнениями для удобочитаемости кода и макросами для синтаксического сахара.

---
+++ include/syntax_sugar.h
@@ -0,0 +1,27 @@
+/**
+ * @file syntax_sugar.h
+ * @author Ilya Biro (st.tell@mail.ru)
+ * @brief Дополнения для документирования кода и синтаксический сахар.
+ * @version 1.0
+ * @date 2022-04-03
+ * 
+ * @copyright Copyright (c) 2022
+ * 
+ */
+
+#ifndef SYNTAX_SUGAR_H
+#define SYNTAX_SUGAR_H
+
+/**
+ * @brief Обозначение для входных параметров функции
+ */
+#define IN
+
+/**
+ * @brief Обозначение для выходных параметров функции
+ */
+#define OUT
+
+
+
+#endif // SYNTAX_SUGAR_H
